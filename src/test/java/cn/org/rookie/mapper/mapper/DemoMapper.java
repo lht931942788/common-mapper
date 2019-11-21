264c981f35e24076ed94ba05fb8e9a637278308c
931942788@qq.com <a19941104>
2019-11-21 13:09:19 +0800

    更新

--- src/test/java/cn/org/rookie/mapper/mapper/DemoMapper.java
+++
@@ -1,9 +0,0 @@
-package cn.org.rookie.mapper.mapper;
-
-import cn.org.rookie.mapper.BaseMapper;
-import cn.org.rookie.mapper.entity.Demo;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DemoMapper extends BaseMapper<Demo, String> {
-}
