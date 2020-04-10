c51b789326750fb0007bfd0e2c80bf35374ed4d4
931942788@qq.com <a19941104>
2020-04-10 09:25:24 +0800

    优化SQLBuilder

--- src/main/java/cn/org/rookie/mapper/sql/SQLBuilder.java
+++ src/main/java/cn/org/rookie/mapper/sql/SQLBuilder.java
@@ -1,10 +1,7 @@
 package cn.org.rookie.mapper.sql;
 
 import cn.org.rookie.mapper.sql.where.Condition;
-import cn.org.rookie.mapper.table.AssociationInfo;
-import cn.org.rookie.mapper.table.ColumnInfo;
-import cn.org.rookie.mapper.table.JoinColumnInfo;
-import cn.org.rookie.mapper.table.TableInfo;
+import cn.org.rookie.mapper.table.*;
 import org.apache.ibatis.jdbc.SQL;
 
 import java.util.List;
@@ -23,40 +20,44 @@ public class SQLBuilder {
     public SQLBuilder insert() {
         List<ColumnInfo> columns = tableInfo.getColumns();
         sql = new SQL();
-        sql.INSERT_INTO(tableInfo.getTableName());
+        sql.INSERT_INTO(getTableName());
         StringBuilder value = new StringBuilder();
         StringBuilder values = new StringBuilder();
         for (int i = 0; i < columns.size(); i++) {
             ColumnInfo columnInfo = columns.get(i);
+            String columnName = columnInfo.getColumnName();
+            String fieldName = columnInfo.getFieldName();
             if (i + 1 == columns.size()) {
-                value.append(String.format(ifScript(columnInfo.getFieldName(), false), columnInfo.getColumnName()));
-                values.append(String.format(ifScript(columnInfo.getFieldName(), false), "#{" + columnInfo.getFieldName() + "}"));
+                value.append(ifScript(fieldName, columnName, ""));
+                values.append(ifScript(fieldName, sharp(fieldName), ""));
             } else {
-                value.append(String.format(ifScript(columnInfo.getFieldName(), true), columnInfo.getColumnName()));
-                values.append(String.format(ifScript(columnInfo.getFieldName(), true), "#{" + columnInfo.getFieldName() + "}"));
+                value.append(ifScript(fieldName, columnName, ","));
+                values.append(ifScript(fieldName, sharp(fieldName), ","));
             }
         }
-        sql.VALUES(tableInfo.getPrimaryInfo().getColumnName(), "#{" + tableInfo.getPrimaryInfo().getFieldName() + "}");
+        sql.VALUES(getPrimaryColumnName(), sharp(getPrimaryFieldName()));
         sql.VALUES(value.toString(), values.toString());
         return this;
     }
 
     public SQLBuilder delete() {
-        sql = new SQL().DELETE_FROM(tableInfo.getTableName());
+        sql = new SQL().DELETE_FROM(getTableName());
         return this;
     }
 
     public SQLBuilder update() {
         List<ColumnInfo> columns = tableInfo.getColumns();
         sql = new SQL();
-        sql.UPDATE(tableInfo.getTableName());
+        sql.UPDATE(getTableName());
         StringBuilder set = new StringBuilder();
         for (int i = 0; i < columns.size(); i++) {
             ColumnInfo columnInfo = columns.get(i);
+            String fieldName = columnInfo.getFieldName();
+            String columnName = columnInfo.getColumnName();
             if (i + 1 == columns.size()) {
-                set.append(String.format(ifScript(columnInfo.getFieldName(), false), columnInfo.getColumnName() + " = #{" + columnInfo.getFieldName() + "}"));
+                set.append(ifScript(fieldName, set(columnName, fieldName), ""));
             } else {
-                set.append(String.format(ifScript(columnInfo.getFieldName(), true), columnInfo.getColumnName() + " = #{" + columnInfo.getFieldName() + "}"));
+                set.append(ifScript(fieldName, set(columnName, fieldName), ","));
             }
         }
         sql.SET(set.toString());
@@ -65,25 +66,26 @@ public class SQLBuilder {
 
     public SQLBuilder select() {
         sql = new SQL();
-        String tableName = tableInfo.getTableName();
-        sql.SELECT(tableName + "." + tableInfo.getPrimaryInfo().getColumnName() + " \"" + tableInfo.getPrimaryInfo().getFieldName() + "\"");
+        String tableName = getTableName();
+        sql.SELECT(select(tableName, getPrimaryColumnName(), getPrimaryFieldName()));
         for (ColumnInfo columnInfo : tableInfo.getColumns()) {
-            sql.SELECT(tableName + "." + columnInfo.getColumnName() + " \"" + columnInfo.getFieldName() + "\"");
+            sql.SELECT(select(tableName, columnInfo.getColumnName(), columnInfo.getFieldName()));
         }
         sql.FROM(tableName);
         for (JoinColumnInfo joinColumnInfo : tableInfo.getJoinColumns()) {
-            sql.FROM(joinColumnInfo.getTableName());
-            sql.SELECT(joinColumnInfo.getTableName() + "." + joinColumnInfo.getColumnName() + " \"" + joinColumnInfo.getFieldName() + "\"");
+            String joinTableName = joinColumnInfo.getTableName();
+            sql.FROM(joinTableName);
+            sql.SELECT(select(joinTableName, joinColumnInfo.getColumnName(), joinColumnInfo.getFieldName()));
             List<AssociationInfo> associations = joinColumnInfo.getAssociations();
             for (AssociationInfo associationInfo : associations) {
-                sql.WHERE(tableName + "." + associationInfo.getTarget() + " = " + joinColumnInfo.getTableName() + "." + associationInfo.getAssociation());
+                sql.WHERE(tableName + "." + associationInfo.getTarget() + " = " + joinTableName + "." + associationInfo.getAssociation());
             }
         }
         return this;
     }
 
     public SQLBuilder byPrimary() {
-        sql.WHERE(tableInfo.getTableName() + "." + tableInfo.getPrimaryInfo().getColumnName() + " = #{id}");
+        sql.WHERE(getTableName() + "." + getPrimaryColumnName() + " = #{id}");
         return this;
     }
 
@@ -115,11 +117,36 @@ public class SQLBuilder {
         return "<script>" + sql.toString() + "</script>";
     }
 
-    private String ifScript(String fieldName, boolean b) {
-        if (b) {
-            return "<if test=\"" + fieldName + " != null" + " and " + fieldName + " != ''\">%s,</if>";
-        } else {
-            return "<if test=\"" + fieldName + " != null" + " and " + fieldName + " != ''\">%s</if>";
-        }
+    private String ifScript(String fieldName, String content, String split) {
+        return "<if test=\"" + fieldName + " != null and " + fieldName + " != ''\">" + content + split + "</if>";
     }
+
+    private String getTableName() {
+        return tableInfo.getTableName();
+    }
+
+    private PrimaryInfo getPrimaryInfo() {
+        return tableInfo.getPrimaryInfo();
+    }
+
+    private String getPrimaryColumnName() {
+        return getPrimaryInfo().getColumnName();
+    }
+
+    private String getPrimaryFieldName() {
+        return getPrimaryInfo().getFieldName();
+    }
+
+    private String sharp(String content) {
+        return String.format("#{%s}", content);
+    }
+
+    private String select(String tableName, String columnName, String fieldName) {
+        return String.format("%s.%s \"%s\"", tableName, columnName, fieldName);
+    }
+
+    private String set(String columnName, String fieldName) {
+        return String.format("%s = #{%s}", columnName, fieldName);
+    }
+
 }
